67a51c639d701a2f4ea30a3399c3ea58bdf06b0a
Chia Wei Jie <schiaweijie@gmail.com>
2018-01-25 07:12:43 +0800

    Started building the model

--- data/data_cleaner.py
+++ data/data_cleaner.py
@@ -46,25 +46,112 @@ def replace_rare_characters(df, column, to_replace, replace_with, new_col_name):
     df[new_col_name] = df[column].str.replace(replace_this, replace_with)
     
     return df
-    
 
+def clean_and_obtain_chars(df, column, replace_with, new_col_name):
+    '''
+    Wrapper function that replaces fairly rare characters in the text.
+    Also returns dictionary of wanted (common) characters
+    Arguments are the same as replace_rare_characters
+    '''
+    character_counts = find_all_unique_characters(df, column)
+    wanted_characters = character_counts.loc[character_counts > 20000] #Completely arbitrary
+    wanted_characters_keys = "".join(wanted_characters.sort_index().keys())
+    
+    clean_comments = replace_rare_characters(df, column,
+                            wanted_characters_keys, replace_with, new_col_name)
+    
+    wanted_chars = find_all_unique_characters(clean_comments, new_col_name)
+    
+    return clean_comments, wanted_chars
+
+#=====Functions that help to generate the character embeddings=====
+    
+def create_mapping_dict(wanted_chars):
+    '''
+    Creates mapping dicts of characters to indices and indices to characters
+    Also adds in an index for the end token
+    wanted_chars: Series of wanted characters IN DESIRED INDEX ORDER
+    '''
+    char_list = list(wanted_chars.keys())
+    char_idx_dict = {char_list[i]:i for i in range(len(char_list))}    
+    idx_char_dict = {v:k for k,v in char_idx_dict.items()}
+    
+    return char_idx_dict, idx_char_dict
+    
+def create_char_array(wanted_chars):
+    '''
+    Creates one hot array of characters
+    wanted_chars: Dictionary of key=char, value = frequency generated from clean_and_obtain_chars
+    '''
+    char_array = pd.get_dummies(pd.Series(wanted_chars.keys()))
+    return char_array[wanted_chars.keys()] #Make sure the ordering is stable
+
+def map_chars(string, char_dict, SEQ_LEN, END_TOKEN):
+    '''
+    Maps characters to their respective indices
+    string: string to map.
+    char_dict: character to index dict
+    seq_len: maximum sequence length that we want
+    '''
+    EFF_SEQ_LEN = SEQ_LEN - 1 #-1 as last place is for end token
+    
+    if len(string)>EFF_SEQ_LEN:
+        desired_str = string[:EFF_SEQ_LEN] #Just ignore everything after this
+    else:
+        desired_str = string
+        
+    indexed = [char_dict[char] for char in desired_str]
+    indexed.append(char_dict[END_TOKEN]) #appends the end_token
+    
+    #Append end tokens until we get SEQ_LEN. Makes storing easier
+    string_length = len(indexed)
+    if string_length<SEQ_LEN:
+        difference = SEQ_LEN - string_length
+        end_list = [char_dict[END_TOKEN] for i in range(difference)]
+        indexed.extend(end_list)
+        assert len(indexed)==SEQ_LEN, "Index array is not of the required length"
+        
+    return indexed, string_length
+
+def map_characters_to_indices(df, column_to_map, char_dict, SEQ_LEN, END_TOKEN):
+    '''
+    Function that creates np matrix of character index mappings
+    df: comment_df
+    column_to_map: column of text that you want to do mappings from
+    char_dict: char to index dict
+    '''
+    #For tomorrow. Basically just truncate the sequences by seq_len. then create array of index mappings
+    
+    items = df[column_to_map].apply(lambda x: map_chars(x, char_dict, SEQ_LEN, END_TOKEN))
+    #Items is a series. the values are an array where idx 0 = the indexed str, 1 = the length
+    indices = [i[0] for i in items.values]
+    lengths = [i[1] for i in items.values]
+    
+    return np.array(indices), np.array(lengths)
+    
 if __name__ == "__main__":
+    #=====Load the files=====
     raw_posts_url = r"raw_data/posts_2017_dt.csv"
     raw_posts = pd.read_csv(raw_posts_url, index_col = 0, encoding = "windows-1252")
     
     raw_comments_url = r"raw_data/comments_2017_dt.csv"
     raw_comments = pd.read_csv(raw_comments_url, index_col = 0, encoding = "windows-1252")
-    raw_comments['comment_text'] = raw_comments['comment_text'].str.lower()
+    raw_comments['comment_text'] = raw_comments['comment_text'].str.lower() #Removes caps for now
     
+    #=====Replace rare-characters in the text with nothing=====
     is_english = raw_comments['comment_text'].apply(lambda x: isEnglish(x))
-    raw_comments_subset = raw_comments.loc[is_english]
+    raw_comments_subset = raw_comments.loc[is_english] #Removes entries with non-standard characters
+    clean_comments, wanted_chars = clean_and_obtain_chars(raw_comments_subset, "comment_text",
+                                                          "", "cleaned_text")
+    #=====Create embedding matrices=====
+    END_TOKEN = "~"
+    wanted_chars[END_TOKEN] = 0 #Add an end token into the list
+    char_idx_dict, idx_char_dict = create_mapping_dict(wanted_chars)
+    char_array = create_char_array(wanted_chars)
     
-    character_counts = find_all_unique_characters(raw_comments_subset, "comment_text")
-    wanted_characters = character_counts.loc[character_counts > 20000] #Completely arbitrary
-    wanted_characters_keys = "".join(wanted_characters.sort_index().keys())
+    SEQ_LEN = 350 #maximum sequence length 
+    indices, str_lengths = map_characters_to_indices(clean_comments, "cleaned_text", char_idx_dict,
+                                                        SEQ_LEN, END_TOKEN)
     
-    test_df = replace_rare_characters(raw_comments_subset, "comment_text",
-                            wanted_characters_keys, "", "replaced_text")
     
-    test = find_all_unique_characters(test_df, "replaced_text")
     
---
+++ models/generator_network.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 06:13:31 2018
+
+@author: Moomootank
+"""
+
+import tensorflow as tf
+
+class GeneratorNetwork():
+    #=====Functions that set up the parameters of the network=====
+    def __init__(self, sess, config):
+        self.num_features = config.embed_size 
+        self.num_classes = config.num_classes
+        self.num_batches = config.batch_size #Number of batches you want
+        self.pretrained_embeddings = config.embeddings #Just need to have one copy of all the embeddings
+
+        self.num_epochs =  config.num_epochs
+        self.learning_rate = config.learning_rate
+        self.num_layers = config.num_layers 
+        self.log_location = config.log_location #Where you want to save the intermediate models
+        
+        self.session = sess #tensorflow session
+    
+    def define_network_hyperparams(self, n_hidden_units, n_dropout, n_input_dropout):
+        #Placed in another function instead of init to allow for easier
+        #parameter optimization
+        self.num_hidden_units = n_hidden_units
+        self.num_dropout = n_dropout
+        self.num_input_dropout = n_input_dropout
+    
+    #=====Functions that set up the basic structure of the network, such as data=====
+    def add_placeholders(self):
+        with tf.name_scope("Data"):
+            self.input_placeholder = tf.placeholder(dtype= tf.float32, shape = (None, self.num_features))
+            self.labels_placeholder = tf.placeholder(dtype= tf.int32, shape = (None))
+            self.input_len_placeholder = tf.placeholder(dtype = tf.int16, shape = (None))
+            #Need dropout to be in placeholder format to make it easier to turn it off during prediction
+            self.dropout_placeholder = tf.placeholder(dtype = tf.float32, shape = (self.num_layers)) 
+            self.input_dropout_placeholder = tf.placeholder(dtype = tf.float32, shape = (self.num_layers))
+               
+    def create_feed_dict(self, inputs_batch, labels_batch, n_dropout, input_len):
+        feed_dict = {self.input_placeholder: inputs_batch, self.labels_placeholder: labels_batch, 
+                     self.dropout_placeholder: n_dropout,
+                     self.input_len_placeholder: input_len }
+        return feed_dict
+    
+    def add_cells(self):
+        assert len(self.num_hidden_units)==len(self.num_dropout)
+        assert len(self.num_hidden_units)==self.num_layers
+        
+        forward_cells = []
+        init = tf.contrib.layers.xavier_initializer(uniform = True, dtype= tf.float32)
+        
+        for i in range(self.num_layers):
+            without_dropout = tf.contrib.rnn.LSTMCell(self.num_hidden_units[i], activation = tf.relu, initializer = init)
+            one_layer = tf.contrib.rnn.DropoutWrapper(without_dropout, 
+                                                      output_keep_prob = 1 - self.dropout_placeholder[i])
+            forward_cells.append(one_layer)
+    
+        multi_forward_cell = tf.contrib.rnn.MultiRNNCell(forward_cells)
+        return multi_forward_cell
+    
+    def add_embeddings(self):
+        #Deal with this later
+        with tf.name_scope("Data"):
+            embedding = tf.nn.embedding_lookup(params = self.pretrained_embeddings, ids = self.input_placeholder)
+        return tf.cast(embedding, dtype = tf.float32)
+    
+    #=====Functions that run the model=====
+    def prediction_op(self):
+        init = tf.contrib.layers.xavier_initializer(uniform = True, dtype= tf.float32)
+        x = self.input_placeholder
+        
+        cells = self.add_cells()
+        outputs, state = tf.nn.dynamic_rnn(cells, inputs = x, sequence_length = self.input_len_placeholder, dtype = tf.float32)
+            
+        '''
+        outputs: Tensor shaped: [batch_size, max_time, cell.output_size]
+        cell.output_size == self.num_hidden_units[-1] (hopefully lol)
+        '''
+        #class_weights: going to be the hidden size of the last layer, and num_classes = cell.output_size
+        class_weights = tf.get_variable("class_weights", shape = (self.num_hidden_units[-1], self.num_classes))
+        class_bias = tf.get_variable("class_bias", initializer = init, shape = (self.num_classes))
+        
+        predictions = tf.matmul(outputs, class_weights) + class_bias #[batch_size, max_time, self.num_classes]
+        return predictions
+        
+    def loss_op(self, predictions):
+        with tf.name_scope("loss_ops"):
+            #Think carefully about the loss_op!!!!
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = self.labels_placeholder, logits = predictions)
+            loss = tf.reduce_mean(loss)
+        return loss
+    
+    def training_op(self, loss):
+        with tf.name_scope("optimizer"):
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+            train_op = optimizer.minimize(loss)      
+        return train_op
+    
+    def initialize_ops(self):
+        self.add_placeholders() # Add the placeholders
+        self.pred = self.prediction_op() #Add the prediction op
+        self.loss = self.loss_op(self.pred)
+        self.train = self.training_op(self.loss)
